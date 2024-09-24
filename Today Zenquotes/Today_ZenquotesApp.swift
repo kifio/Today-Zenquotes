4f30e47263b9452e079eed689a67fc8fabac1ff9
Ivan Murashov <iamurashov@yandex.ru>
2024-09-24 23:58:55 +0300

    Initial Commit

---
+++ Today Zenquotes/Today_ZenquotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Today_ZenquotesApp.swift
+//  Today Zenquotes
+//
+//  Created by Иван Мурашов on 23.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct Today_ZenquotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
